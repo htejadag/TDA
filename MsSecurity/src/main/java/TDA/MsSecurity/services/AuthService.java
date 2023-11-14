7c434e908178d6d30e1e6f40f452d0767f5bbdfe
htejadag <htejadag25@gmail.com>
2023-11-14 10:03:30 -0500

    Update AuthService.java
    Implementar servicios crud

--- MsSecurity/src/main/java/TDA/MsSecurity/services/AuthService.java
+++ MsSecurity/src/main/java/TDA/MsSecurity/services/AuthService.java
@@ -1,21 +1,45 @@
 package TDA.MsSecurity.services;
 
 import java.util.List;
- 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
  
-import TDA.MsSecurity.model.modelUsuario;
+import TDA.MsSecurity.model.UsuarioModel;
 import TDA.MsSecurity.repository.IAuthRepository;
  
 @Service
-public class AuthService {
+public class AuthService implements IAuthService {
  
     @Autowired
     IAuthRepository authRepository;
-   
-    public List<modelUsuario>  getAllUsers() {       
-        return (List<modelUsuario>) authRepository.findAll();
+
+    @Override
+    public UsuarioModel add(UsuarioModel model) {
+        return authRepository.save(model);
     }
-   
+
+    @Override
+    public UsuarioModel update(UsuarioModel model) {
+        return authRepository.save(model);
+    }
+
+    @Override
+    public boolean delete(int id) {        
+        authRepository.deleteById(id);
+        return true;
+    }
+
+    @Override
+    public List<UsuarioModel> findAll() {
+        return (List<UsuarioModel>) authRepository.findAll();
+    }
+
+    @Override
+    public UsuarioModel findById(int id) {
+        Optional<UsuarioModel> model = authRepository.findById(id);
+        return model.get();
+    }
+    
 }
