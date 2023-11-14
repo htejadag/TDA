66bad92ce428ca3c6eeef517c3eec38c52f92d16
htejadag <htejadag25@gmail.com>
2023-11-14 10:04:50 -0500

    Update AuthController.java
    Implementar servicios crud en controller

--- MsSecurity/src/main/java/TDA/MsSecurity/controller/AuthController.java
+++ MsSecurity/src/main/java/TDA/MsSecurity/controller/AuthController.java
@@ -2,23 +2,47 @@ package TDA.MsSecurity.controller;
 
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import TDA.MsSecurity.model.modelUsuario;
-import TDA.MsSecurity.services.AuthService;
+import TDA.MsSecurity.model.UsuarioModel;
+import TDA.MsSecurity.services.IAuthService;
 
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController {
 
     @Autowired
-    AuthService authService;
+    IAuthService authService;
 
-    @GetMapping
-    public List<modelUsuario> get() {
-        return authService.getAllUsers();
-    }   
+    @GetMapping("/getAll")
+    public List<UsuarioModel> getAll() {
+        return authService.findAll();
+    } 
+    
+    @GetMapping("/getById")
+    public UsuarioModel getById(int id) {
+        return authService.findById(id);
+    }
+
+    @PostMapping("/create")
+    public UsuarioModel create(@RequestBody UsuarioModel model) {
+        return authService.add(model);
+    }
+
+    @PutMapping("/update")
+    public UsuarioModel update(@RequestBody UsuarioModel model) {
+        return authService.update(model);
+    }
+
+    @DeleteMapping("/delete")
+    public boolean delete(int id) {
+        return authService.delete(id);
+    }
 
 }
